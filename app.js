b566568a2569fcf51ba5d9343b6105cc319ec609
maxo <maxolotikashvili@yahoo.com>
2023-08-02 16:15:03 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+    cors: { origin: '*' }
+});
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) => {
+        if (message !== '') {
+            io.emit('message', message);
+        } else {
+            return;
+        }
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected!');
+    });
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
